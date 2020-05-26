12022b83442d165aefe3149bdf303077186d8425
fantom1408 <52611340+fantom1408@users.noreply.github.com>
2020-05-27 02:57:23 +0300

    добавил модальное окно
    Signed-off-by: fantom1408 <52611340+fantom1408@users.noreply.github.com>

--- script/main.js
+++ script/main.js
@@ -1,8 +1,10 @@
-
 //меню
 
 const leftMenu = document.querySelector('.left-menu');
 const hamburger = document.querySelector('.hamburger');
+const tvShowsList = document.querySelector('.tv-shows__list');
+const modal = document.querySelector('.modal');
+
 
 //открытие/закрытие меню
 
@@ -29,3 +31,34 @@ leftMenu.addEventListener('click', event => {
     
     
 });
+
+//открытие модального окна
+
+tvShowsList.addEventListener('click', event => {
+    const target = event.target;
+    const card = target.closest('.tv-card');
+
+    if(card) {
+        document.body.style.overflow = 'hidden';
+        modal.classList.remove('hide');
+    }
+    console.log('card: ', card);
+    
+});
+
+//закрытие модального окна
+
+modal.addEventListener('click', event => {
+
+    if(event.target.closest('.cross') ||
+        event.target.classList.contains('modal')) {
+        document.body.style.overflow = '';
+        modal.classList.add('hide');
+    }
+
+});
+
+//смена карточки
+
+// tvShowsList.addEventListener('mouseover', )
+// tvShowsList.addEventListener('mouseout', )
