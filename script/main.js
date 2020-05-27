e9b8163a529ae3e1d7d0de4df6907b20a3d700a8
fantom1408 <52611340+fantom1408@users.noreply.github.com>
2020-05-28 00:29:46 +0300

    замена карточек

--- script/main.js
+++ script/main.js
@@ -1,3 +1,4 @@
+
 //меню
 
 const leftMenu = document.querySelector('.left-menu');
@@ -6,15 +7,61 @@ const tvShowsList = document.querySelector('.tv-shows__list');
 const modal = document.querySelector('.modal');
 
 
+const DBService = class {
+    getData = async (url) => {
+        const res = await fetch(url);
+        if (res.ok){
+            return res.json();
+        }else{
+            throw new Error(`Не удалось получить данные по адресу ${url}`);
+        }
+        
+        
+    }
+
+    getTestData = async () => {
+        return await this.getData('test.json');
+    }
+}
+
+const renderCard = response => {
+    console.log(response);
+    tvShowsList.textContent = '';
+
+    response.results.forEach(item => {
+        const card = document.createElement('li')
+        card.className = 'tv-shows__item';
+        card.innerHTML = `
+        <a href="#" class="tv-card">
+            <span class="tv-card__vote">6.4</span>
+            <img class="tv-card__img"
+                src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/xul6SG8rar3wkHPY8YusUtxcdlZ.jpg"
+                data-backdrop="https://image.tmdb.org/t/p/w185_and_h278_bestv2/9eXNA3K010TlkUTLSzQ07cP6uPF.jpg"
+                alt="Звёздные войны: Сопротивление">
+            <h4 class="tv-card__head">Звёздные войны: Сопротивление</h4>
+        </a>
+        `;
+
+        tvShowsList.append(card)
+
+        console.log('card: ', card);
+        
+    });
+}
+
+new DBService().getTestData().then(renderCard);
+// console.log(renderCard);
+
+
 //открытие/закрытие меню
 
 hamburger.addEventListener('click', () => {
-    leftMenu.classList.toggle('openMenu');    
+    leftMenu.classList.toggle('openMenu');
     hamburger.classList.toggle('open');
 });
 
 document.addEventListener('click', (event) => {
-    if(!event.target.closest('.left-menu')){
+    if (!event.target.closest('.left-menu')) {
         leftMenu.classList.remove('openMenu');
         hamburger.classList.remove('open');
     }
@@ -28,29 +75,32 @@ leftMenu.addEventListener('click', event => {
         leftMenu.classList.add('openMenu');
         hamburger.classList.add('open');
     }
-    
-    
+
+
 });
 
 //открытие модального окна
 
 tvShowsList.addEventListener('click', event => {
+
+    event.preventDefault();
+
     const target = event.target;
     const card = target.closest('.tv-card');
 
-    if(card) {
+    if (card) {
         document.body.style.overflow = 'hidden';
         modal.classList.remove('hide');
     }
     console.log('card: ', card);
-    
+
 });
 
 //закрытие модального окна
 
 modal.addEventListener('click', event => {
 
-    if(event.target.closest('.cross') ||
+    if (event.target.closest('.cross') ||
         event.target.classList.contains('modal')) {
         document.body.style.overflow = '';
         modal.classList.add('hide');
@@ -59,6 +109,28 @@ modal.addEventListener('click', event => {
 });
 
 //смена карточки
+const changeImage = event => {
+    const card = event.target.closest('.tv-shows__item');
+    if (card) {
+        const img = card.querySelector('.tv-card__img');
 
-// tvShowsList.addEventListener('mouseover', )
-// tvShowsList.addEventListener('mouseout', )
+                //можно сделать так
+
+        if (img.dataset.backdrop) {
+            [img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src];
+        }
+            // а можно сделать так, написано ниже, выполняет тоже самое
+
+        /*const changeImg = img.dataset.backdrop;
+        if(changeImg) {
+            img.dataset.backdrop = img.src;
+            img.src = changeImg;
+        }*/
+    }
+
+};
+
+tvShowsList.addEventListener('mouseover', changeImage);
+tvShowsList.addEventListener('mouseout', changeImage);
+
+
