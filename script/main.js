7bd1a41ca540486af1929d7c83a290a6396bf4bd
fantom1408 <52611340+fantom1408@users.noreply.github.com>
2020-05-29 03:49:02 +0300

    3 preloader

--- script/main.js
+++ script/main.js
@@ -1,15 +1,35 @@
 
 const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
-const API_KEY = '2097eceedba965021905edcf0e4ae709';
+// const SERVER = 'https://api.themoviedb.org/3';
+// const API_KEY = '2097eceedba965021905edcf0e4ae709';
+
 //меню
 
 const leftMenu = document.querySelector('.left-menu');
 const hamburger = document.querySelector('.hamburger');
 const tvShowsList = document.querySelector('.tv-shows__list');
 const modal = document.querySelector('.modal');
+const tvShows = document.querySelector('.tv-shows');
+const tvCardImg = document.querySelector('.tv-card__img');
+const modalTitle = document.querySelector('.modal__title');
+const genreList = document.querySelector('.genres-list');
+const rating = document.querySelector('.rating');
+const description = document.querySelector('.description');
+const modalLink = document.querySelector('.modal__link');
+const searchForm = document.querySelector('.search__form');
+const searchFormInput = document.querySelector('.search__form-input');
+const preloader = document.querySelector('.preloader');
+
+const loading = document.createElement('div');
+loading.className = 'loading';
 
 
-const DBService = class {
+DBService = class {
+
+    constructor(){
+        this.SERVER = 'https://api.themoviedb.org/3';
+        this.API_KEY = '2097eceedba965021905edcf0e4ae709';
+    }
     getData = async (url) => {
         const res = await fetch(url);
         if (res.ok){
@@ -17,59 +37,89 @@ const DBService = class {
         }else{
             throw new Error(`Не удалось получить данные по адресу ${url}`);
         }
-        
-        
+                
     }
 
     getTestData = () => {
-        return  this.getData('test.json');
+        return this.getData('test.json');
     }
+    
+    getTestCard = () => {
+        return this.getData('card.json');
+    }
+
+    getSearchResult = query => 
+        this.getData(`${this.SERVER}/search/tv?api_key=${this.API_KEY}&language=ru-RU&query=${query}`);
+        
+    getTvShow = id => this.getData(`${this.SERVER}/tv/${id}?api_key=${this.API_KEY}&language=ru-RU`);
+    
 }
+// console.log(new DBService().getSearchResult('ooo'));
+
 
 const renderCard = response => {
     console.log(response);
     tvShowsList.textContent = '';
 
                 //можно так написать
-    // response.results.forEach(item => {
-    //     const { backdrop_path: backdrop,
-    //             name: title,
-    //             poster_path: poster,
-    //             vote_average: vote
-    //         } = item;
+    response.results.forEach(item => {
+        const { 
+            backdrop_path: backdrop,
+            name: title,
+            poster_path: poster,
+            vote_average: vote,
+            id
+            } = item;
 
                 //или так
-        response.results.forEach(({ backdrop_path: backdrop,
-                                    name: title,
-                                    poster_path: poster,
-                                    vote_average: vote
-                                    })  => {
+        // response.results.forEach(({ backdrop_path: backdrop,
+        //                             name: title,
+        //                             poster_path: poster,
+        //                             vote_average: vote,
+        //                             id
+        //                             })  => {
 
         const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
         const backdropIMG = backdrop ? IMG_URL + backdrop : '';
         const voteElem = vote ? `<span class="tv-card__vote">${vote}</span>` : '';        
 
-        const card = document.createElement('li')
+        const card = document.createElement('li');
+        card.idTV = id;
+        // console.dir(card);
         card.className = 'tv-shows__item';
         card.innerHTML = `
-        <a href="#" class="tv-card">
-            <span class="tv-card__vote">${vote}</span>
+        <a href="#" id="${id}" class="tv-card">
+            ${voteElem}
             <img class="tv-card__img"
                 src="${posterIMG}"
-                data-backdrop="${IMG_URL + backdrop}"
+                data-backdrop="${backdropIMG}"
                 alt="${title}">
             <h4 class="tv-card__head">${title}</h4>
         </a>
         `;
-
+        loading.remove();
         tvShowsList.append(card);
 
-        console.log('card: ', card);
+        
         
     });
-}
+};
 
-new DBService().getTestData().then(renderCard);
+searchForm.addEventListener('submit', event => {
+    event.preventDefault();    
+    const value = searchFormInput.value.trim();
+    if(value){
+        tvShows.append(loading);
+        new DBService().getSearchResult(value).then(renderCard);
+    }
+    searchFormInput.value = '';
+    
+
+    // console.log(value);
+});
+
+
+
 // console.log(renderCard);
 
 
@@ -88,6 +138,7 @@ document.addEventListener('click', (event) => {
 });
 
 leftMenu.addEventListener('click', event => {
+    event.preventDefault();
     const target = event.target;
     const dropdown = target.closest('.dropdown');
     if (dropdown) {
@@ -109,10 +160,34 @@ tvShowsList.addEventListener('click', event => {
     const card = target.closest('.tv-card');
 
     if (card) {
-        document.body.style.overflow = 'hidden';
-        modal.classList.remove('hide');
+
+        preloader.style.display = 'block';
+
+        new DBService().getTvShow(card.id)
+        .then(data => {
+            // console.log(data);
+            tvCardImg.src = IMG_URL + data.poster_path;
+            tvCardImg.alt = data.name;
+            modalTitle.textContent = data.name;
+            genreList.textContent = '';
+            data.genres.forEach(item => {
+                genreList.innerHTML += `<li>${item.name}</li>`;
+            })
+            
+            rating.textContent = data.vote_average;
+            description.textContent = data.overview;
+            modalLink.href = data.homepage;
+
+        })
+            .then(() => {
+                document.body.style.overflow = 'hidden';
+                modal.classList.remove('hide');
+            })
+        .then(() => {
+            preloader.style.display = '';
+        })
     }
-    console.log('card: ', card);
+    
 
 });
 
